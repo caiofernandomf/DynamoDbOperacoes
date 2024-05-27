a998a409b1e2d054e98750a3df808ba8f407023a
caiofernandomf <caiofernandomf@gmail.com>
2024-05-27 17:08:23 -0300

    refactor: usando tableDescription para construir consultas e delete

--- src/main/java/br/lab/caiofernadomf/DynamoDbOperacoes/repository/operations/DeleteItem.java
+++ src/main/java/br/lab/caiofernadomf/DynamoDbOperacoes/repository/operations/DeleteItem.java
@@ -6,11 +6,13 @@ import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ConditionalOperator;
 import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.xspec.Condition;
 import com.amazonaws.services.dynamodbv2.xspec.DeleteItemExpressionSpec;
 import com.amazonaws.services.dynamodbv2.xspec.ExpressionSpecBuilder;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DeleteItem {
@@ -23,16 +25,22 @@ public class DeleteItem {
         DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
         Table table = dynamoDB.getTable(tableName);
 
+        List<KeySchemaElement> keySchemaElementList=
+                table.describe().getKeySchema();
+
+        PrimaryKey primaryKey = new PrimaryKey();
+
         Map mapaItem = GetItem.getItem(amazonDynamoDB,tableName,keyName,keyValue);
 
-        DeleteItemSpec deleteItemSpec = new DeleteItemSpec().withPrimaryKey(keyName,keyValue,"ip",mapaItem.get("ip"));
-
-        DeleteItemOutcome deleteItemOutcome=null;
         if(!mapaItem.isEmpty()){
 
-            deleteItemOutcome= table.deleteItem(deleteItemSpec);
+            keySchemaElementList.forEach(k-> primaryKey.addComponent(k.getAttributeName(),mapaItem.get(k.getAttributeName())));
 
-            return deleteItemOutcome.getItem().asMap();
+            DeleteItemSpec deleteItemSpec = new DeleteItemSpec().withPrimaryKey(primaryKey);
+
+            table.deleteItem(deleteItemSpec);
+
+            return mapaItem;
         }
 
         return new HashMap();
--- src/main/java/br/lab/caiofernadomf/DynamoDbOperacoes/repository/operations/ReadTable.java
+++ src/main/java/br/lab/caiofernadomf/DynamoDbOperacoes/repository/operations/ReadTable.java
@@ -3,13 +3,15 @@ package br.lab.caiofernadomf.DynamoDbOperacoes.repository.operations;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.internal.IteratorSupport;
+import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.xspec.ScanExpressionSpec;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ReadTable {
 
@@ -24,9 +26,24 @@ public class ReadTable {
 
         Table table = dynamoDB.getTable(tableName);
 
-        ScanFilter scanFilter = new ScanFilter("ip").exists();
+        List<KeySchemaElement> keySchemaElementList=
+        table.describe().getKeySchema();
 
-        IteratorSupport<Item, ScanOutcome> iterator=   table.scan(scanFilter).iterator();
+        Map<String,String> mapaKey = new HashMap<>();
+
+        keySchemaElementList.forEach(keySchemaElement ->
+            mapaKey.put("#attr".concat(keySchemaElement.getAttributeName()), keySchemaElement.getAttributeName())
+                );
+
+        ScanSpec scanSpec = new ScanSpec();
+
+        String filterExp =mapaKey.keySet().stream().map(k->"attribute_exists(".concat(k).concat(")")).collect(Collectors.joining(" and "));
+
+        scanSpec.withFilterExpression(filterExp);
+
+        scanSpec.withNameMap(mapaKey);
+
+        IteratorSupport<Item, ScanOutcome> iterator=   table.scan(scanSpec).iterator();
         List<Map> resultList = new ArrayList<>();
 
         while (iterator.hasNext()){
